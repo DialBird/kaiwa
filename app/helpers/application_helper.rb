e35e2d8ef813dbdc4997be6b0080fdf269bd67a9
Keisuke <rememberthatk@icloud.com>
2018-11-10 03:39:30 +0000

    setup meta_tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      charset: 'utf-8',
+      title: 'Kaiwa',
+      viewport: 'width=device-width, initial-scale=1.0'
+    }
+  end
 end
