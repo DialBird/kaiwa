5e052e3b2620ab38dd3047784b2f2863db7fddac
Keisuke <rememberthatk@icloud.com>
2018-11-11 00:41:55 +0000

    add flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
       viewport: 'width=device-width, initial-scale=1.0'
     }
   end
+
+  def display_flash
+    flash.each do |type, msg|
+      # Deviseのセッションタイムアウト時のflashは無視する
+      next if type == 'timedout'
+
+      type = 'success' if type == 'notice'
+      type = 'error' if %w[alert danger].include? type
+
+      concat content_tag(:script, "toastr.#{type}(\"#{msg}\");".html_safe)
+    end
+    nil
+  end
 end
