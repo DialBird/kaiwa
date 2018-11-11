4041d14383029d67502e49d28d2e893de172c10b
Keisuke <rememberthatk@icloud.com>
2018-11-11 03:03:50 +0000

    Add logout button to header

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    delete 'logout' => 'users/sessions#destroy', as: :destroy_user_session
+  end
 
   get 'dashboard' => 'dashboard#index'
 
