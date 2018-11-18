7b8dba5d7ca5c6835d904dd7a689f91eb10ff7b3
Keisuke <rememberthatk@icloud.com>
2018-11-18 03:18:29 +0000

    Edit dashborad view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  def index
+  permits :current_goal_id, model_name: 'User'
+
+  def index(user = nil)
     @user = current_user
+    @user.update(user) if user
     @goal = @user.current_goal
   end
 end
